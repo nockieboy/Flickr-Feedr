ca660e3a76b11f37b1a64dd82fdcb41f0c8dbcfa
Jonathan Nock <nockieboy@gmail.com>
2015-11-17 21:40:56 +0000

    Added 'back to top' button
    Floating 'back to top' button added to return user to top of document
    where the title and search bar is.  Button only appears when not at top
    of page.

--- bb_code.js
+++ bb_code.js
@@ -150,3 +150,17 @@ app.GetUsername = function(userID, caller)
     }
 	});
 }
+
+$('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Back to Top</div>');
+$(window).scroll(function () {
+  if ($(this).scrollTop() != 0) {
+		$('#toTop').fadeIn();
+	} else {
+		$('#toTop').fadeOut();
+	}
+});
+
+$('#toTop').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+});
